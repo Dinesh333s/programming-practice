477303ec1abcc63bac815abfc791ab9326e20d34
dinesh <dinesh.it16@bitsathy.ac.in>
2019-09-11 00:17:52 +0530

    new added

--- c/arrrev.c
+++ c/arrrev.c
@@ -2,15 +2,16 @@
 #include<stdlib.h>
 int main()
 {
-int n,i;
-scanf("%d",&n);
-int *arr = (int*)malloc(n * sizeof(int));
-for (i=0;i<n;i++)
-{
-scanf("%d",&arr[i]);
-}
-for(i=n-1;i>=0;i--)
-{
-printf("%d ",(arr[i]));
-}
+  int n,i;
+  scanf("%d",&n);
+  int *arr = (int*)malloc(n * sizeof(int));
+  for (i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+
+  for(i=n-1;i>=0;i--)
+  {
+      printf("%d ",(arr[i]));
+  }
 }
